b4c84ff66afc1ab34d51830a6d7925e6b337a180
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-06-14 13:49:49 -0500

    navbar init

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,19 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export const Navbar = () => {
-    return (
-        <nav className="">
-            <div>
-                <Link href="/">Woven</Link>
-            </div>
-            <div>
-                <Link href="/">Home</Link>
-                <Link href="/products">Products</Link>
-                <Link href="/checkout">Checkout</Link>
-            </div>
-            <div>
-                
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav className="sticky top-0 z-50 bg-white shadow">
+      <div className="container mx-auto flex items-center justify-between px-4 py-4">
+        <Link href="/" className="hover:text-blue-600">
+          Woven
+        </Link>
+        <div className="hidden md:flex space-x-6">
+          <Link href="/">Home</Link>
+          <Link href="/products" className="hover:text-blue-600">Products</Link>
+          <Link href="/checkout" className="hover:text-blue-600">Checkout</Link>
+        </div>
+        <div className="flex items-center space-x-4"></div>
+      </div>
+    </nav>
+  );
+};
