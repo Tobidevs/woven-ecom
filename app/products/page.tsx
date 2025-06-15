67d60bc7b7eefbb0c00d52178ba045d1fbd73770
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-06-15 13:41:55 -0500

    Create filtered Products functionality

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -5,6 +5,7 @@ export default async function Products() {
     const products = await stripe.products.list({
         expand: ["data.default_price"],
     });
+    
     return (
         <div className="pb-8">
             <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">
